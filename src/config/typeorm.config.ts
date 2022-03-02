05c6e0b22b22b8587a63c034b9a3b93f52e7993b
SinaHaseli <sina.haseli@gmail.com>
2022-03-02 11:48:11 +0330

    init

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,5 +1,4 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { isTest } from '../app/base/constants';
 
 export const TypeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -7,10 +6,9 @@ export const TypeOrmConfig: TypeOrmModuleOptions = {
   port: Number(process.env.DATABASE_PORT) || 5432,
   username: process.env.DATABASE_USERNAME || 'postgres',
   password: process.env.DATABASE_PASSWORD || 'postgres',
-  database: isTest ? process.env.DATABASE_TEST_NAME : process.env.DATABASE_NAME,
+  database: process.env.DATABASE_NAME,
   synchronize: true,
   autoLoadEntities: true,
-  logging: process.env.DATABASE_LOG === 'true',
   dropSchema: false,
   extra: {
     max: 40,
